feff5e2d14eaee5edd45a6e95d67da9cb9f05bd5
Heang Virith <virith.id@gmail.com>
2024-11-16 11:37:40 +0700

    fetch destination data on route change; update DestinationShowView to handle async data loading

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div class="home">
     <the-navigation></the-navigation>
     <div class="container">
+      <!-- 
+        Use key to force re-render of the component 
+        when the route changes, so that the component
+        is re-mounted and the data is re-fetched.
+      -->
+
+      <!-- <router-view :key="$route.path"></router-view> -->
       <router-view></router-view>
     </div>
   </div>
--- src/views/DestinationShowView.vue
+++ src/views/DestinationShowView.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="destination">
+    <section v-if="destination" class="destination">
         <h1>{{ destination.name }}</h1>
         <div class="destination-details">
             <img :src="`/images/${destination.image}`" :alt="destination.description">
@@ -9,14 +9,19 @@
 </template>
 
 <script setup>
-import sourceData from '@/data.json'
-import { computed, ref } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute();
-const destinations = ref(sourceData.destinations);
+const destination = ref(null);
 
-const destination = computed(() => {
-    return destinations.value.find(destination => destination.id === +route.params.destination);
-});
+const initData = async () => {
+    const response = await fetch(`https://travel-dummy-api.netlify.app/${route.params.slug}`);
+    destination.value = await response.json();
+};
+
+onMounted(initData);
+
+watch(() => route.params.slug, initData);
+
 </script>
